cca4181d173f37e5ad8e9f1b84d6662448969989
Christian <christian.hendricks@gmx.de>
2019-01-05 11:33:09 +0100

    Change return value.

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 
-    return jsonify({'data': repr([1])})
+    return jsonify({'data': repr([1337])})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
